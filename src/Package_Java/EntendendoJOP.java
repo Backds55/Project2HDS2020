5005b48522b187c44ed8f17df6d9f73243bfe0cf
FAM <>
2020-08-18 22:33:50 -0300

    Entendendo JOP - janelas Java - string, doubleparse, soma;

---
+++ src/Package_Java/EntendendoJOP.java
@@ -0,0 +1,30 @@
+package Package_Java;
+
+//importar as bibliotecas para uso na classe
+import javax.swing.*;
+
+public class EntendendoJOP {
+    
+    public static void main(String args[]){
+        //declaração das variáveis locais
+        double num1, num2, resp=0;
+        String aux=""; //Inicializei uma string com vazio("");
+        
+        aux = JOptionPane.showInputDialog(null, "Insira o primeiro número:");
+        //conversão de tipos utilizando parse():
+        //converte tipo string para double (valor necessário);
+        num1 = Double.parseDouble(aux);
+        
+        aux = JOptionPane.showInputDialog(null, "Insira o segundo número:");
+        num2 = Double.parseDouble(aux);
+        
+        resp = num1+num2;
+        JOptionPane.showMessageDialog(null, "A soma é = "+resp);
+                
+                
+                
+                
+                
+        
+   }
+}
